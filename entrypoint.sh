8e7fb3788cdeddb785fd130c3b5fde3521037667
Johannes Hamfler <jh@z7k.de>
2019-05-07 15:40:18 +0200

    fixed ip var error

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ fi
 
 echo starting UE
 echo ma-ue --target "https://""$TARGETDOMAIN"":4430/amfstart"
-echo ma-ue --target "https://""$TARGETIP"":4430/amfstart"
+echo ma-ue --target "https://$ip:4430/amfstart"
 cd /usr/local/bin/
 echo $(pwd)
-ma-ue --target "https://""$TARGETIP"":4430/amfstart"
+ma-ue --target "https://$ip:4430/amfstart"
